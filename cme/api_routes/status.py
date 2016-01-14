fc004269ae8207c991ec762d4fca612918939c85
James Brunner <james.brunner@kaelus.com>
2016-01-13 17:24:50 -0700

    Combine some routes in status to reduce code duplication

--- cme/api_routes/status.py
+++ cme/api_routes/status.py
@@ -32,15 +32,9 @@ def set_control_state(ch_index, control_index, state):
 	mc.set('status', json.dumps(hw))
 
 
-def status():
-	''' top-level CME status object '''
-
-	# try to read temperature (could fail if not on RPi)
-	# temp in millidegrees C
-	try:
-		temp_C = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
-	except:
-		temp_C = -40.0 # Not on a RPi
+def status(ch_index=-1):
+	''' Top-level CME status object
+	'''
 
 	# Update the channels objects with the hardware data (from memcache).
 	# I found that sometimes the mc.get('status') was returning None which
@@ -49,6 +43,23 @@ def status():
 	# channels.
 	cme_status = mc.get('status')	
 	status = json.loads(cme_status) if cme_status else { 'channels': [] }
+
+	# select a particular channel and/or sensor/control?
+	if not ch_index < 0:
+		if not (ch_index >= 0 and ch_index < len(status['channels'])):
+			return None
+
+		return Channel(status['channels'][ch_index])
+
+	# If we reach here we return entire status object, including
+	# the CPU temperature and timestamp.
+
+	# try to read temperature (could fail if not on RPi)
+	# temp in millidegrees C
+	try:
+		temp_C = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
+	except:
+		temp_C = -40.0 # Not on a RPi
 
 	return {
 		'timestamp': datetime.utcnow().isoformat() + 'Z',
@@ -76,15 +87,14 @@ def raw():
 @router.route('/ch/<int:ch_index>', methods=['GET', 'POST'])
 @router.route('/ch/<int:ch_index>/name', methods=['GET', 'POST'])
 @router.route('/ch/<int:ch_index>/description', methods=['GET', 'POST'])
+@router.route('/ch/<int:ch_index>/controls/')
+@router.route('/ch/<int:ch_index>/sensors/')
 @require_auth
 def channel(ch_index):
-	s = status()
+	ch = status(ch_index)
 
-	if not (ch_index >= 0 and ch_index < len(s['channels'])):
+	if not ch:
 		return json_error('Channel not found', 404)
-
-	# get the requested channel
-	ch = s['channels'][ch_index]
 
 	# parse out the item name (last element of request path)
 	segments = UriParse.path_parse(request.path)
@@ -106,89 +116,69 @@ def channel(ch_index):
 		return json_response({ ch.id: { 'name': ch.name }})
 	elif item == 'description':
 		return json_response({ ch.id: { 'description': ch.description }})
+	elif item == 'controls':
+		return json_response({ ch.id + ':controls': ch.controls })
+	elif item == 'sensors':
+		return json_response({ ch.id + ':sensors': ch.sensors })
 	else:
 		return json_response({ ch.id: ch })
 
 
-@router.route('/ch/<int:ch_index>/controls/')
-@router.route('/ch/<int:ch_index>/sensors/')
+@router.route('/ch/<int:ch_index>/sensors/<int:sc_index>')
+@router.route('/ch/<int:ch_index>/controls/<int:sc_index>', methods=['GET', 'POST'])
+@router.route('/ch/<int:ch_index>/sensors/<int:sc_index>/name', methods=['GET', 'POST'])
+@router.route('/ch/<int:ch_index>/controls/<int:sc_index>/name', methods=['GET', 'POST'])
+@router.route('/ch/<int:ch_index>/controls/<int:sc_index>/state', methods=['GET', 'POST'])
 @require_auth
-def sensors_and_controls(ch_index):
-	s = status()
-	
-	if not (ch_index >= 0 and ch_index < len(s['channels'])):
+def sensor_control(ch_index, sc_index):
+
+	ch = status(ch_index)
+
+	if not ch:
 		return json_error('Channel not found', 404)
 
-	# parse out the item name (last element of request path)
+	# parse out the item (name, state) and the item type (sensor or control)
 	segments = UriParse.path_parse(request.path)
 	item = segments[-1].lower()
-	
-	ch = s['channels'][ch_index]
 
-	if item == 'controls':
-		obj = { ch.id + ':controls': ch.controls }
+	if item == 'name' or item == 'state':
+		typename = segments[-3].lower()
 	else:
-		obj = { ch.id + ':sensors': ch.sensors }
+		typename = segments[-2].lower()
 
-	return json_response(obj)
+	# retrieve the object by type and index
+	if typename == 'sensors':
+		if not (sc_index >= 0 and sc_index < len(ch.sensors)):
+			return json_error('Sensor not found', 404)
 
+		obj = ch.sensors[sc_index]
+	
+	elif typename == 'controls':
+		if not (sc_index >= 0 and sc_index < len(ch.controls)):
+			return json_error('Control not found', 404)
 
-@router.route('/ch/<int:ch_index>/sensors/<int:s_index>')
-@require_auth
-def sensor(ch_index, s_index):
-	s = status()
-
-	if not (ch_index >= 0 and ch_index < len(s['channels'])):
-		return json_error('Channel not found', 404)
-
-	ch = s['channels'][ch_index]
-
-	if not (s_index >= 0 and s_index < len(ch.sensors)):
-		return json_error('Sensor not found', 404)
-
-	sen = ch.sensors[s_index]
-
-	return json_response({ ch.id + ':' + sen.id: sen })
+		obj = ch.controls[sc_index]
 
 
-@router.route('/ch/<int:ch_index>/controls/<int:c_index>', methods=['GET', 'POST'])
-@router.route('/ch/<int:ch_index>/controls/<int:c_index>/name', methods=['GET', 'POST'])
-@router.route('/ch/<int:ch_index>/controls/<int:c_index>/state', methods=['GET', 'POST'])
-@require_auth
-def control(ch_index, c_index):
-	s = status()
-
-	if not (ch_index >= 0 and ch_index < len(s['channels'])):
-		return json_error('Channel not found', 404)
-
-	ch = s['channels'][ch_index]
-
-	if not (c_index >= 0 and c_index < len(ch.controls)):
-		return json_error('Control not found', 404)
-
-	ctrl = ch.controls[c_index]
-
-	# parse out the item name (last element of request path)
-	segments = UriParse.path_parse(request.path)
-	item = segments[-1].lower()
-
-	# update name or description (or both) from POST data
+	# update name or state (or both) from POST data
 	if request.method == 'POST':
-		ctrl_update = request.get_json()
+		update = request.get_json()
+
 		if item == 'name':
-			ctrl.name = ctrl_update.get('name', ctrl.name)
+			obj.name = update.get('name', obj.name)
 		elif item == 'state':
-			ctrl.state = ctrl_update.get('state', ctrl.state)
-			set_control_state(ch_index, c_index, ctrl.state)
+			obj.state = update.get('state', obj.state)
+			set_control_state(ch_index, sc_index, obj.state)
 		else:
-			ctrl.name = ctrl_update.get('name', ctrl.name)
-			ctrl.state = ctrl_update.get('state', ctrl.state)
-			set_control_state(ch_index, c_index, ctrl.state)
+			obj.name = update.get('name', obj.name)
+			if typename == 'control':
+				obj.state = update.get('state', obj.state)
+				set_control_state(ch_index, sc_index, obj.state)
 
 	# figure out what to return
 	if item == 'name':
-		return json_response({ ch.id + ':' + ctrl.id: { 'name': ctrl.name }})
+		return json_response({ ch.id + ':' + obj.id: { 'name': obj.name }})
 	elif item == 'state':
-		return json_response({ ch.id + ':' + ctrl.id: { 'state': ctrl.state }})
+		return json_response({ ch.id + ':' + obj.id: { 'state': obj.state }})
 	else:
-		return json_response({ ch.id + ':' + ctrl.id: ctrl })
+		return json_response({ ch.id + ':' + obj.id: obj })
