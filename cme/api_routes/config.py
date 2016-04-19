9ae9550827caba88f07c804a14c517e24cca65d9
James Brunner <james.brunner@kaelus.com>
2016-04-19 13:46:05 -0600

    Move the /device API route into the top-level config route (for now).  It uses the common refresh_device() call to check for update images.

--- cme/api_routes/config.py
+++ cme/api_routes/config.py
@@ -1,11 +1,96 @@
-# CME configuration API route handlers
-from . import router, request, settings
+# CME device and configuration API route handlers
+from . import app, router, settings, request, UriParse, secure_filename
 
 from .auth import require_auth
 from .util import json_response, json_error, json_filter
 from ..util.ClockUtils import refresh_time
 
 import os, time, threading
+
+# Read-only device settings - NOT password protected
+# These are saved in settings, but under the "__device" key
+@router.route('/device/')
+@router.route('/device/modelNumber')
+@router.route('/device/serialNumber')
+@router.route('/device/firmware')
+def device_read_only_settings():
+	# parse out the setting name (last element of request path)
+	segments = UriParse.path_parse(request.path)
+	item = segments[-1]
+
+	# device requests need to check for update files
+	refresh_device()
+
+	if item == 'device':
+		return json_response({ 'device': json_filter(settings['__device'].items()) })
+	else:
+		return json_response({ item: settings['__device'][item] })
+
+
+# update firmware (POST file or path to /config/device/update)
+@router.route('/device/update', methods=['GET', 'POST'])
+@require_auth
+def device_update():
+	filename = settings['__device']['__update']
+
+	if request.method == 'POST':
+		# handle upload new firmware
+		file = request.files['file']
+
+		if file and __allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+
+			path = path.join(app.config['UPLOADS'], filename)
+			print("saving uploads to {", p, "}")
+
+			file.save(p)
+
+	refresh_device()
+
+	return json_response({ 'update': filename })
+
+
+# trigger a firmware update
+@router.route('/device/updateTrigger', methods=['POST'])
+@require_auth
+def device_trigger_update():
+	return json_error([ 'Not implemented' ])
+
+
+# DEBUGGING
+# Use this method to debug file uploads
+@router.route('/device/up', methods=['GET', 'POST'])
+@require_auth
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and __allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+
+            p = os.path.join(app.config['UPLOADS'], filename)
+
+            print ("Saved upload to `", p, "`")
+            file.save(p)
+
+            return json_response({ 'update': filename })
+
+	# on GET just return simple form for uploading a file
+    return'''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>'''
+# END DEBUGGING
+
+
+# check allowed filename extensions
+def __allowed_file(filename):
+	return '.' in filename and \
+			filename.rsplit('.', 1)[1].lower() in [x.lower() for x in app.config['ALLOWED_EXTENSIONS']]
+
 
 # top-level configuration
 @router.route('/config/', methods=['GET', 'POST'])
@@ -26,7 +111,7 @@ def factoryReset():
 	
 	# Factory reset deletes the settings.json file and performs a 
 	# reboot.
-	t = threading.Thread(target=factory_reset, args=(5,))
+	t = threading.Thread(target=factory_reset, args=(5, False, False, ))
 	t.setDaemon(True)
 	t.start()
 
@@ -49,7 +134,7 @@ def refresh_device():
 
 
 # perform the factory reset 
-def factory_reset(delay=5):
+def factory_reset(delay=5, reset_network=False, reset_clock=False):
 
 	try:
 		os.remove(app.config['SETTINGS'])
