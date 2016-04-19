c58558885c133dd0a844557c8c0d46a81f737e52
James Brunner <james.brunner@kaelus.com>
2016-04-19 13:16:14 -0600

    Removed FileUtils (it was not really appropriately utilized) and moved it's functions into config API route handler

--- cme/api_routes/config.py
+++ cme/api_routes/config.py
@@ -1,13 +1,11 @@
 # CME configuration API route handlers
-
 from . import router, request, settings
 
 from .auth import require_auth
 from .util import json_response, json_error, json_filter
-from ..util.FileUtils import refresh_device, delay_factory_reset
 from ..util.ClockUtils import refresh_time
 
-import threading
+import os, time, threading
 
 # top-level configuration
 @router.route('/config/', methods=['GET', 'POST'])
@@ -28,9 +26,41 @@ def factoryReset():
 	
 	# Factory reset deletes the settings.json file and performs a 
 	# reboot.
-	t = threading.Thread(target=delay_factory_reset, args=(5,))
+	t = threading.Thread(target=factory_reset, args=(5,))
 	t.setDaemon(True)
 	t.start()
 
 	# Return nothing (but status = 200) to let 'em know we're resetting
 	return json_response(None)
+
+
+
+# check for firmware update file presence
+def refresh_device():
+	''' Check uploads folder for any contents.  There should only
+		be at most a single file which will be used if an update
+		is triggered.'''
+
+	files = [fn for fn in os.listdir(app.config['UPLOADS'])
+			if any(fn.endswith(ext) for ext in app.config['ALLOWED_EXTENSIONS'])]
+
+	# choose the first one, if any
+	settings['__device']['__update'] = '' if len(files) == 0 else files[0]
+
+
+# perform the factory reset 
+def factory_reset(delay=5):
+
+	try:
+		os.remove(app.config['SETTINGS'])
+	except:
+		pass
+
+	print("Factory reset and restart in {0} seconds...".format(delay))
+	time.sleep(delay)
+
+	if app.config['IS_CME']:
+		os.system("reboot")
+	
+
+
