597ee7b13a24f360a7103f017632f6c04fa3c3a7
James Brunner <james.brunner@kaelus.com>
2016-04-26 17:16:48 -0700

    Get rid of the legacy "learning how to make this work" stuff.

--- cme/ui_routes/index.py
+++ cme/ui_routes/index.py
@@ -5,13 +5,3 @@ from . import app, settings, router, render_template, APIError
 @router.route('/index')
 def index():
 	return render_template('index.html', title='Avalanche')
-
-
-@router.route('/hello')
-def hello():
-	return render_template('hello.html', title='Flask + ReactJS')
-
-
-@router.route('/err')
-def errors():
-	return render_template('error.html', error=APIError('Error'))
