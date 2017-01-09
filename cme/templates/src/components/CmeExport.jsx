58b1737401ddfb3cc18fc42b1e0e6bddb2ebdd53
James Brunner <james.brunner@smithsmicrowave.com>
2017-01-09 14:30:52 -0700

    Start to build up the process flow for data export

--- cme/templates/src/components/CmeExport.jsx
+++ cme/templates/src/components/CmeExport.jsx
@@ -7,13 +7,47 @@
  */
 var React = require('react');
 
+
+// loads the page's query string into an object, qs
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
+
 var CmeExport = React.createClass({
+
+	getInitialState: function () {
+		return {
+			ch: qs['c'],
+			history: qs['h'],
+			data: []
+		};
+	},
+	
+	componentDidMount: function() {
+		alert('Hey, I am mounted!');
+	},
 
 	render: function() {
 
 		return (
 			<div>
-				This is my export page!
+				<div>
+					Channel: {this.state.ch}
+				</div>
+				<div>
+					History: {this.state.history}
+				</div>
 			</div>
 		);
 	}
