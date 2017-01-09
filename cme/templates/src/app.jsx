11441d2603b5d60f3d78a94f258ce380d52cb11e
James Brunner <james.brunner@smithsmicrowave.com>
2017-01-09 13:46:00 -0700

    Fiddling with the app structure to handle another html page cleanly.

--- cme/templates/src/app.jsx
+++ cme/templates/src/app.jsx
@@ -11,15 +11,27 @@ var ReactDOM = require('react-dom');
 var Actions = require('./Actions');
 var CmeApp = require('./components/CmeApp');
 
-// Initialize the CmeStore.  This is written to more easily support
-// running the web app from a separate server than the Cme API.  Most
-// Cme API requests take success and failure callbacks.  These end up
-// being CmeActions that get dispatched and handled by the CmeStore.
+// This script is also called as the entry point for
+// the export.html page.  In that case there is nothing
+// really to do, so just return.
+if (document.getElementById('cme-export')) {
 
-// Wait for these requests to complete - they initialize the Store.
-Actions.device()
-	.always(Actions.login()
-		.always(function () {
-			ReactDOM.render(<CmeApp />, document.getElementById('cmeapp'));
-		})
-	);
+	alert('This is the export page!');
+
+} else {
+
+
+	// Initialize the CmeStore.  This is written to more easily support
+	// running the web app from a separate server than the Cme API.  Most
+	// Cme API requests take success and failure callbacks.  These end up
+	// being CmeActions that get dispatched and handled by the CmeStore.
+
+	// Wait for these requests to complete - they initialize the Store.
+	Actions.device()
+		.always(Actions.login()
+			.always(function () {
+				ReactDOM.render(<CmeApp />, document.getElementById('cmeapp'));
+			})
+		);
+
+}
