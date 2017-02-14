f254decd0bb2d00f26367f33847d2feefc7506f6
James Brunner <james.brunner@smithsmicrowave.com>
2017-02-14 09:21:40 -0700

    Updates the build system for API application and recovery layers.
    The recovery API layer is built into an archive (.tgz) using the cme-build-recovery.sh script.
    
    The application API layer (docker) is built using a 2-step process.  First, wheels are built using the cme-build.docker, then the run-time container is built from those wheels using cmi-run.docker.

---
+++ build/cme-build-recovery.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Run this script to build a Cme (API layer) distribution tarball
+# that can be downloaded to a CME device and installed to provide
+# the recovery mode operation.
+
+
+# Requires Cme source folder at /root/Cme/
+SRC=/root/Cme
+
+# Source files copied to SRCDIST
+SRCDIST=/root/srcdist
+
+# System built to DIST
+DIST=/root/dist
+
+# Point PIP env paths to wheelhouse
+export WHEELHOUSE=${DIST}/wheelhouse
+export PIP_WHEEL_DIR=${DIST}/wheelhouse
+export PIP_FIND_LINKS=${DIST}/wheelhouse
+
+mkdir ${SRCDIST}
+mkdir ${DIST}
+mkdir ${WHEELHOUSE}
+
+# Copy source files over to srcdist/
+# Note: this is to avoid wheel adding a bunch of files and
+# directories that are not needed in the distribution.
+pushd ${SRCDIST}
+cp -R ${SRC}/cme/ .
+cp ${SRC}/VERSION .
+cp ${SRC}/setup.py .
+cp ${SRC}/MANIFEST.in .
+
+# Activate the Cme venv
+source ${SRC}/cme_venv/bin/activate
+
+# Generate the wheels for the application.
+# These will show up in WHEELHOUSE
+pip wheel .
+
+# Copy the top-level VERSION file
+cp ${SRCDIST}/VERSION ${DIST}/VERSION
+
+# Wheels are built - done with srcdist/
+popd
+rm -rf ${SRCDIST}
+
+# Now generate the archive of the wheels
+pushd ${DIST}
+
+# Read the VERSION file to use in the created archive name
+VERSION=$(<VERSION)
+ARCHIVE=1500-000-v$VERSION-SWARE-CME_RECOVERY.tgz
+
+tar -czvf ../${ARCHIVE} .
+
+# Done with the built distribution
+popd
+rm -rf ${DIST}
+
+echo "Done!"
+exit 0
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import os
 from setuptools import setup
 
 # version (i.e., cme.firmware) is stored in the VERSION file in package root
-#with open(os.path.join(os.getcwd(), 'VERSION')) as f:
-#	version = f.readline().strip()
+with open(os.path.join(os.getcwd(), 'VERSION')) as f:
+	version = f.readline().strip()
 
 setup (
 	name					= "cme",
-	version					= "0.1",
-	description 			= "CME API and web UI",
+	version					= version,
+	description 			= "CME API with built-in web UI",
 	packages				= ['cme', 'cme.api_routes', 'cme.ui_routes', 'cme.common'],
 	include_package_data	= True,
 	zip_safe				= False,
