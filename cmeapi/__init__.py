822d0952d1f2d2da41c1cb5c685b9ad300b2ca8b
James Brunner <james.brunner@smithsmicrowave.com>
2017-04-25 12:12:20 -0600

    Use the package name as the Flask application name.  Uses __name__ so we don’t have to change in multiple locations in the future.

--- cmeapi/__init__.py
+++ cmeapi/__init__.py
@@ -1,4 +1,4 @@
 # Adding this here so submodules can import 'app' (from .. import app) to
 # access the various Flask routines.
 from flask import Flask
-app = Flask('cme-api')
+app = Flask(__name__) # use the package name, "cmeapi"
